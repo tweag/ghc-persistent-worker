b82f94ef309ce959c0e6665357e47017f375ecde
Torsten Schmits <git@tryp.io>
2026-03-12 14:25:00 +0100

    Move all tests to `buck-worker-internal`
    Change-Id: I67e48fd66f6bc5835beae9be5c610b4e08ca4a5e

--- internal/internal-test/Main.hs
+++ internal/internal-test/Main.hs
@@ -1,14 +1,22 @@
 module Main where
 
 import BuildPlanTest (test_buildPlan_make, test_buildPlan_oneshot)
-import Test.Tasty (TestTree, defaultMain, testGroup)
+import CompileHptTest (test_compileHpt)
+import ProjectBuildTest (test_projectBuild)
+import ScheduleTest (test_sortScheduleOrder)
+import Test.Data.Env (testConfigOptions)
+import Test.Tasty (TestTree, defaultIngredients, defaultMainWithIngredients, includingOptions, testGroup)
 
 tests :: TestTree
 tests =
   testGroup "all" [
     test_buildPlan_make,
-    test_buildPlan_oneshot
+    test_buildPlan_oneshot,
+    test_compileHpt,
+    test_sortScheduleOrder,
+    test_projectBuild
   ]
 
 main :: IO ()
-main = defaultMain tests
+main =
+  defaultMainWithIngredients (includingOptions testConfigOptions : defaultIngredients) tests
