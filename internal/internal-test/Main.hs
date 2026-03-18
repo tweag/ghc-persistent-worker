8e1a85db56203432ce87941389a0001790b560c8
Torsten Schmits <git@tryp.io>
2026-03-18 13:42:53 +0100

    Test compat with vanilla GHC
    Change-Id: I41eeb109d15168caeeb22e8953581053dc2ca261

--- internal/internal-test/Main.hs
+++ internal/internal-test/Main.hs
@@ -1,21 +1,35 @@
-module Main where
+{-# LANGUAGE CPP #-}
 
-import BuildPlanTest (test_buildPlan_make, test_buildPlan_oneshot)
+module Main where
+ 
+import BuildPlanTest (test_buildPlan)
 import CompileHptTest (test_compileHpt)
 import ProjectBuildTest (test_projectBuild)
 import ScheduleTest (test_sortScheduleOrder)
 import Test.Data.Env (testConfigOptions)
 import Test.Tasty (TestTree, defaultIngredients, defaultMainWithIngredients, includingOptions, testGroup)
+ 
+fullTest :: Bool
+
+#if defined(MWB) || defined(MWB_2025_10)
+
+fullTest = True
+
+#else
+
+fullTest = False
+
+#endif
 
 tests :: TestTree
 tests =
-  testGroup "all" [
-    test_buildPlan_make,
-    test_buildPlan_oneshot,
-    test_compileHpt,
+  testGroup "all" $ [
     test_sortScheduleOrder,
     test_projectBuild
-  ]
+  ] <> if fullTest then [
+    test_buildPlan,
+    test_compileHpt
+  ] else []
 
 main :: IO ()
 main =
