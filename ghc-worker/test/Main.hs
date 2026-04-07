f2d776a7f4075fc4869cc1ace73eab80fbc690d4
Torsten Schmits <git@tryp.io>
2026-04-07 18:00:03 +0200

    WIP: profiling test
    Change-Id: I4985bac361001beb024c58ebebbf78006c5d0db9

--- ghc-worker/test/Main.hs
+++ ghc-worker/test/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 import BuildPlanTest (test_buildPlan)
 import CompileHptTest (test_compileHpt)
+import ProfileTest (test_profiling)
 import ProjectBuildTest (test_projectBuild)
 import ResourceTest (test_resources)
 import ScheduleTest (test_sortScheduleOrder)
@@ -50,7 +51,8 @@ tests :: TestTree
 tests =
   testGroup "all" [
     test_resources,
-    afterResources (testGroup "general" testsGeneral)
+    afterResources (testGroup "general" testsGeneral),
+    afterResources test_profiling
   ]
   where
     -- tasty 1.5 has @sequentialTestGroup@, but the current Nix env has 1.4, so we'll make do with this for now.
--- ops/package-sets.nix
+++ ops/package-sets.nix
@@ -89,6 +89,20 @@ in {
 
   envs.profiled = defaultEnv [({notest, ...}: { ghc-worker = notest; ghc-server = notest; })];
 
+  envs.profiled-25-10 = let
+
+    overrides = {notest, ...}: {
+      ghc-worker = notest;
+      ghc-server = notest;
+    };
+
+  in {
+    hls.enable = lib.mkForce false;
+    overrides = commonOverrides ["mwb-25-10"] ++ [ipeOverrides overrides];
+    package-set.extends = "mwb-25-10";
+    profiling = true;
+  };
+
   # This environment is for building the worker with an externally provided, impure GHC.
   envs.cabal-build = {
     expose.shell = true;
@@ -134,6 +148,7 @@ in {
       bitwise = force;
       boring = force;
       cborg = force;
+      extra = hackage "1.8.1" "0q3hyffi11dazq9n25r508spvmblx21wipfw10hfkxcazv5l1pg2";
       foldl = force;
       generic-deriving = force;
       ghc-source-gen = github {
