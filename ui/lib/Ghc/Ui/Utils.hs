4d304302d277a9ca63988b1430e6c960a0b2b3f3
Torsten Schmits <git@tryp.io>
2026-09-05 17:16:08 +0200

    Move `popup` to a separate module
    Change-Id: I4bb95ef9a685a490e54d000648469babc2c08af8

--- ui/lib/Ghc/Ui/Utils.hs
+++ ui/lib/Ghc/Ui/Utils.hs
@@ -1,18 +1,6 @@
 module Ghc.Ui.Utils where
 
-import Brick (Widget)
-import Brick.Widgets.Border (borderWithLabel)
-import Brick.Widgets.Center (centerLayer)
-import Brick.Widgets.Core (hLimitPercent, str, vLimitPercent)
 import Data.Sequence qualified as Seq
-import Ghc.Ui.Data.Name (Name)
-
-popup :: Int -> String -> Widget Name -> Widget Name
-popup size popupTitle content =
-  centerLayer $
-    hLimitPercent size $
-      vLimitPercent size $
-        borderWithLabel (str $ " " ++ popupTitle ++ " ") content
 
 upsertAscSeq :: (Ord b) => (a -> b) -> a -> Seq.Seq a -> (Int, Seq.Seq a)
 upsertAscSeq meas x as = binSearch 0 (Seq.length as - 1)
