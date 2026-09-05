5f80d5cfa7fbde5f355a9f3bb2ade5041f04f722
Torsten Schmits <git@tryp.io>
2026-09-05 17:02:44 +0200

    Refactor main event handlers in the UI
    Change-Id: I3bb29c8fdea325054db23c44ed759e06cd9df36e

--- ui/lib/Ghc/Ui/Event/Main.hs
+++ ui/lib/Ghc/Ui/Event/Main.hs
@@ -1,13 +1,15 @@
 module Ghc.Ui.Event.Main where
 
-import Brick.Forms (FormFieldState, editTextField, formState, handleFormEvent, newForm, (@@=))
+import Brick.Forms (FormFieldState, editTextField, formState, newForm, (@@=))
 import Brick.Main (getVtyHandle, halt, suspendAndResume')
 import Brick.Types (BrickEvent (..), EventM)
 import Brick.Widgets.Core (txt, (<+>))
 import Brick.Widgets.List (listSelectedElementL)
-import Control.Exception (handle)
 import Control.Monad.IO.Class (liftIO)
-import Data.Foldable (for_)
+import Data.Foldable (traverse_)
+import Data.Map (Map)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (maybeToList)
 import Data.Monoid (First (..))
 import Data.Time (UTCTime (..), fromGregorian)
 import Ghc.Ui.Data.Main (MainEvent (..), MainState (..))
@@ -16,14 +18,14 @@ import Ghc.Ui.Data.ServerApi (ServerApi (..))
 import Ghc.Ui.Data.Session (SessionState, Worker (..))
 import qualified Ghc.Ui.Data.Sessions as Sessions
 import Ghc.Ui.Data.WorkerId (WorkerId (..))
-import Ghc.Ui.Event.Popup (handleListEventOf)
+import Ghc.Ui.Event.Popup (focus, handleForm, handleListEventOf, listKeyEvent, openPopup, popupKeyEvent)
 import qualified Ghc.Ui.Event.Sessions as Sessions
 import Ghc.Ui.Event.Tasks qualified as Tasks
 import Ghc.Ui.GhcDebug (debug)
 import Ghc.Ui.ModuleSelector qualified as ModuleSelector
 import Graphics.Vty (Event (..), Key (..), Output (..), Vty (..))
 import Internal.Debug (debugSocketPath)
-import Lens.Micro.Platform (Traversal', _2, each, filtered, packed, preuse, use, zoom, (.=))
+import Lens.Micro.Platform (Traversal', _2, packed, use, zoom, (.=))
 import Types.State (Options (..), defaultOptions)
 import Types.Target (TargetSpec)
 
@@ -33,6 +35,7 @@ initialState =
     sessions = Sessions.initialState,
     options = newForm optionFields defaultOptions,
     currentFocus = ModuleSelector,
+    previousFocus = ModuleSelector,
     currentTime = UTCTime (fromGregorian 1970 1 1) 0
   }
 
@@ -43,7 +46,8 @@ optionFields =
 currentSession :: Traversal' MainState SessionState
 currentSession = #sessions . listSelectedElementL . _2
 
-beep :: EventM Name MainState ()
+-- TODO replace with op log
+beep :: EventM n s ()
 beep = do
   vty <- getVtyHandle
   liftIO $ vty.outputIface.ringTerminalBell
@@ -65,85 +69,112 @@ withTarget = withTarget' False
 withTargetForRebuild :: (WorkerId -> TargetSpec -> EventM Name MainState ()) -> EventM Name MainState ()
 withTargetForRebuild = withTarget' True
 
-handleEvent :: ServerApi -> BrickEvent Name MainEvent -> EventM Name MainState ()
-handleEvent api@ServerApi {..} = \case
-  (AppEvent (SetTime t)) ->
+selectWorkers :: Maybe WorkerId -> Map WorkerId Worker -> [Worker]
+selectWorkers = \case
+  Just target -> maybeToList . Map.lookup target
+  Nothing -> Map.elems
+
+forWorkers ::
+  Maybe WorkerId ->
+  (Worker -> EventM Name MainState ()) ->
+  EventM Name MainState ()
+forWorkers spec f = do
+  workers <- selectWorkers spec <$> use (currentSession . #workers)
+  traverse_ f workers
+
+sendOptions ::
+  ServerApi ->
+  Maybe WorkerId ->
+  EventM Name MainState ()
+sendOptions api spec = do
+  opts <- use #options
+  forWorkers spec \ worker ->
+    liftIO $ api.sendOptions worker.connection (formState opts)
+
+triggerBuild :: ServerApi -> WorkerId -> TargetSpec -> EventM Name MainState ()
+triggerBuild api workerId target = do
+  forWorkers (Just workerId) \ worker ->
+    liftIO $ api.triggerRebuild worker.connection target
+
+handleMainEvent :: ServerApi -> MainEvent -> EventM Name MainState ()
+handleMainEvent api = \case
+  SetTime t ->
     #currentTime .= t
-  AppEvent (SendOptions mwid) -> do
-    opts <- use #options
-    workers <- use (currentSession . #workers)
-    let workers' = case mwid of
-          Nothing -> workers
-          Just wid -> filter (\ w -> w.workerId == wid) (workers :: [Worker])
-    for_ workers' $ \worker -> do
-      liftIO $
-        handle @IOError (\_ -> pure ()) $
-          sendOptions worker.connection (formState opts)
-  AppEvent (TriggerRebuild wid target) -> do
-    mworker <- preuse (currentSession . #workers . each . filtered (\ w -> w.workerId == wid))
-    for_ mworker $ \worker -> do
-      liftIO $ triggerRebuild worker.connection target
-  AppEvent (SessionSelectorEvent evt) ->
+
+  SendOptions target ->
+    sendOptions api target
+
+  TriggerRebuild worker target ->
+    triggerBuild api worker target
+
+  SessionSelectorEvent evt ->
     zoom #sessions (Sessions.handleEvent evt)
-  VtyEvent evt -> do
+
+handleGlobalKey :: ServerApi -> Name -> Event -> Key -> EventM Name MainState ()
+handleGlobalKey api current event = \case
+  KEsc -> halt
+
+  KChar 'q' -> halt
+
+  KChar 's' ->
+    openPopup Sessions
+
+  KChar 'o' ->
+    openPopup OptionsEditor
+
+  KChar 'd' ->
+    withTarget \ _ target ->
+      suspendAndResume' $ debug (debugSocketPath target)
+
+  KChar 'r' ->
+    withTargetForRebuild \ wid target ->
+      handleEvent api (AppEvent (TriggerRebuild wid target))
+
+  KChar '\t' ->
+    focus case current of
+      Tasks -> ModuleSelector
+      ModuleSelector -> Tasks
+      _ -> current
+
+  KEnter ->
+    -- This ensures the cursor is not on a separator
+    -- TODO improve
+    withTarget \ _ _ ->
+      openPopup case current of
+        Tasks -> TaskDetails
+        ModuleSelector -> ModuleDetails
+        _ -> current
+
+  _ -> case current of
+    Tasks -> handleListEventOf (currentSession . #activeTasks) event
+    ModuleSelector -> handleListEventOf (currentSession . #modules) event
+    _ -> pure ()
+
+vtyEvent :: ServerApi -> Name -> Event -> EventM Name MainState ()
+vtyEvent api = \case
+  Sessions ->
+    listKeyEvent #sessions True
+
+  OptionsEditor ->
+    popupKeyEvent True (handleForm #options) do
+      sendOptions api Nothing
+
+  -- TODO why does this use a list event handler? (and ModuleDetails)
+  TaskDetails ->
+    listKeyEvent (currentSession . #activeTasks) False
+
+  ModuleDetails ->
+    listKeyEvent (currentSession . #modules) False
+
+  current -> \case
+    event@(EvKey key []) -> handleGlobalKey api current event key
+    _ -> pure ()
+
+handleEvent :: ServerApi -> BrickEvent Name MainEvent -> EventM Name MainState ()
+handleEvent api = \case
+  AppEvent event -> handleMainEvent api event
+  VtyEvent event -> do
     current <- use #currentFocus
-    case current of
-      Sessions -> do
-        let hide = #currentFocus .= ModuleSelector
-        case evt of
-          EvKey KEsc [] -> hide
-          EvKey KEnter [] -> hide
-          EvKey (KChar 's') [] -> hide
-          _ -> handleListEventOf #sessions evt
-      OptionsEditor -> do
-        let
-          hide = do
-            #currentFocus .= ModuleSelector
-            handleEvent api (AppEvent (SendOptions Nothing))
-        case evt of
-          EvKey KEsc [] -> hide
-          EvKey KEnter [] -> hide
-          _ -> zoom #options (handleFormEvent (VtyEvent evt))
-      TaskDetails -> do
-        let hide = #currentFocus .= Tasks
-        case evt of
-          EvKey KEsc [] -> hide
-          EvKey KEnter [] -> hide
-          _ -> handleListEventOf (currentSession . #activeTasks) evt
-      ModuleDetails -> do
-        let hide = #currentFocus .= ModuleSelector
-        case evt of
-          EvKey KEsc [] -> hide
-          EvKey KEnter [] -> hide
-          _ -> handleListEventOf (currentSession . #modules) evt
-      _ -> case evt of
-        EvKey KEsc [] -> halt
-        EvKey (KChar 'q') [] -> halt
-        EvKey (KChar 's') [] -> do
-          #currentFocus .= Sessions
-        EvKey (KChar 'o') [] -> do
-          #currentFocus .= OptionsEditor
-        EvKey (KChar 'd') [] -> do
-          withTarget $ \_wid target ->
-            suspendAndResume' $
-              debug (debugSocketPath target)
-        EvKey (KChar 'r') [] -> do
-          withTargetForRebuild $ \wid target ->
-            handleEvent api (AppEvent (TriggerRebuild wid target))
-        EvKey (KChar '\t') [] -> do
-          #currentFocus .= case current of
-            Tasks -> ModuleSelector
-            ModuleSelector -> Tasks
-            _ -> current
-        EvKey KEnter [] -> do
-          withTarget \_ _ ->
-            #currentFocus .= case current of
-              Tasks -> TaskDetails
-              ModuleSelector -> ModuleDetails
-              _ -> current
-        _ -> case current of
-          Tasks -> handleListEventOf (currentSession . #activeTasks) evt
-          ModuleSelector -> handleListEventOf (currentSession . #modules) evt
-          _ -> pure ()
+    vtyEvent api current event
   MouseDown {} -> pure ()
   MouseUp {} -> pure ()
