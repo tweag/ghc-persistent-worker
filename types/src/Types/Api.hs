a2c748c2515b187de210d7523515b59db1307aa8
Torsten Schmits <git@tryp.io>
2026-09-11 15:39:32 +0200

    Add API types for project structure
    Change-Id: I359194761d9968b16d6fea5ff19ee6b6fecc1d78

--- types/src/Types/Api.hs
+++ types/src/Types/Api.hs
@@ -1,8 +1,57 @@
+{-# LANGUAGE NoFieldSelectors #-}
+
 module Types.Api where
 
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Binary (Binary)
 import Data.Map (Map)
+import Data.String (IsString)
+import qualified Data.Text as Text
+import Data.Text (Text)
+import qualified GHC
 import GHC.Generics (Generic)
+import GHC.Unit (Module, mkModuleName, moduleName, moduleNameString, moduleUnitId, unitIdString)
+
+newtype UnitName =
+  UnitName { text :: Text }
+  deriving stock (Eq, Show)
+  deriving newtype (IsString, Ord, Binary, FromJSON, ToJSON)
+
+newtype ModuleName =
+  ModuleName { text :: Text }
+  deriving stock (Eq, Show)
+  deriving newtype (IsString, Ord, Binary, FromJSON, ToJSON)
+
+fromGhcModuleName :: GHC.ModuleName -> ModuleName
+fromGhcModuleName name =
+  ModuleName (Text.pack (moduleNameString name))
+
+toGhcModuleName :: ModuleName -> GHC.ModuleName
+toGhcModuleName name =
+  mkModuleName (Text.unpack name.text)
+
+data HomeModule =
+  HomeModule {
+    unit :: UnitName,
+    name :: ModuleName
+  }
+  deriving stock (Eq, Show, Ord, Generic)
+  deriving anyclass (Binary, FromJSON, ToJSON)
+
+homeModuleFromGhc :: Module -> HomeModule
+homeModuleFromGhc m =
+  HomeModule {
+    unit = UnitName (Text.pack (unitIdString (moduleUnitId m))),
+    name = fromGhcModuleName (moduleName m)
+  }
+
+data UnitSummary =
+  UnitSummary {
+    name :: UnitName,
+    modules :: [ModuleName]
+  }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (Binary)
 
 data Event
   = CompileStart { target :: String, canDebug :: Bool }
