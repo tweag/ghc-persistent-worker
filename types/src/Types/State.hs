8d0e57bb23c74a2c7a1e97637f13cd050c6ade5d
Torsten Schmits <git@tryp.io>
2026-09-12 03:15:20 +0200

    Remove `MakeState.targetArgs`
    Change-Id: Ibf08d165a1882c60bf84556f4cf728cd8d2d559d

--- types/src/Types/State.hs
+++ types/src/Types/State.hs
@@ -1,12 +1,11 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 module Types.State where
 
-import Data.Map.Strict (Map)
 import Data.Set (Set)
 import GHC (HscEnv)
 import System.OsPath (OsPath)
-import Types.Grpc (CommandEnv, RequestArgs)
 import Types.State.Make (MakeState (..))
-import Types.Target (TargetSpec)
 
 data BinPath =
   BinPath {
@@ -19,6 +18,5 @@ data WorkerState =
   WorkerState {
     path :: BinPath,
     baseSession :: Maybe HscEnv,
-    make :: MakeState,
-    targetArgs :: Map TargetSpec (CommandEnv, RequestArgs)
+    make :: MakeState
   }
