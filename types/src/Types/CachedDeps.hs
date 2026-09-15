eb202f546ebae81f63320a5df65b9d12a0a1046c
Torsten Schmits <git@tryp.io>
2026-09-15 14:22:35 +0200

    Make `CachedUnit.is_binary` a `Maybe`
    Change-Id: Ia833f0932a65bda4dcd8a692bbeeb8394d82f7fb

--- types/src/Types/CachedDeps.hs
+++ types/src/Types/CachedDeps.hs
@@ -138,7 +138,7 @@ data CachedUnit =
   CachedUnit {
     build_plan :: Maybe (Map (JsonFs ModuleName) CachedModule),
     cache :: Maybe (Map (JsonFs ModuleName) CachedModule),
-    is_binary :: Bool,
+    is_binary :: Maybe Bool,
     unit_args :: Maybe OsPath,
     unit_buck_args :: Maybe OsPath,
     dep_units :: Maybe OsPath
